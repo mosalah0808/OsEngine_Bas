b1d2c611096d490fd87dbe5ccc17085678dbec33
mosalah0808 <mosalah0808@gmail.com>
2022-07-14 10:04:59 +0300

    t\OsEngine\Robots\PriceChannel_1\PriceChannelFix.cs дз к 20 уроку добавлено

--- OsEngine/Robots/PriceChannel_1/PriceChannelFix.cs
+++ OsEngine/Robots/PriceChannel_1/PriceChannelFix.cs
@@ -19,11 +19,11 @@ namespace OsEngine.Robots.PriceChannel_1
             _tab = TabsSimple[0];
             LenghtUp = CreateParameter("Lenght Channel Up", 12, 5, 80, 2);
             LenghtDown = CreateParameter("Lenght Channel Down", 12, 5, 80, 2);
-            Mode = CreateParameter("Mode", "Off", new[] { "Off", "On"});
+            Mode = CreateParameter("Mode", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" });
             Lot = CreateParameter("Lot", 10, 5, 20, 1);
             Risk = CreateParameter("Risk", 1m, 0.2m, 3.0m, 0.1m);
 
-            _pc = IndicatorsFactory.CreateIndicatorByName("PriceChannelFix",name+ "PriceChannelFix",false);
+            _pc = IndicatorsFactory.CreateIndicatorByName("PriceChannel", name + "PriceChannel",false);
             _pc.ParametersDigit[0].Value = LenghtUp.ValueInt;
             _pc.ParametersDigit[1].Value = LenghtDown.ValueInt;
             _pc = (Aindicator)_tab.CreateCandleIndicator(_pc,"Prime");
@@ -50,6 +50,78 @@ namespace OsEngine.Robots.PriceChannel_1
             if (Mode.ValueString == "Off")
             {
                 return;
+            }
+
+            if (_pc.DataSeries[0].Values==null|| _pc.DataSeries[1].Values == null||
+                _pc.DataSeries[0].Values.Count< LenghtUp.ValueInt + 1||
+                 _pc.DataSeries[1].Values.Count < LenghtDown.ValueInt + 1)
+            {
+                return;
+            }
+
+            Candle candle = candles[candles.Count - 1];
+            decimal lastUp = _pc.DataSeries[0].Values[_pc.DataSeries[0].Values.Count - 2];
+            decimal lastDown = _pc.DataSeries[1].Values[_pc.DataSeries[1].Values.Count - 2];
+            List<Position> positions = _tab.PositionsOpenAll;
+
+            if (Mode.ValueString == "OnlyClosePosition")
+            {
+                return;
+            }
+
+            if (Mode.ValueString != "OnlyShort") //long
+            {
+                if (candle.Close > lastUp && candle.Open < lastUp && positions.Count == 0)
+                {
+                    decimal riskMoney = _tab.Portfolio.ValueBegin * Risk.ValueDecimal / 100;
+                    decimal costPriceStep = _tab.Securiti.PriceStepCost;
+                    costPriceStep = 1;
+                    decimal steps = (lastUp - lastDown) / _tab.Securiti.PriceStep;
+                    decimal lot = riskMoney / (steps * costPriceStep);
+
+                   // _tab.BuyAtMarket((int)lot);
+                    _tab.BuyAtMarket(Lot.ValueInt);
+                }
+            }
+
+            if (Mode.ValueString != "OnlyLong") //short
+            {
+                if (candle.Close < lastDown && candle.Open > lastDown && positions.Count == 0)
+                {
+                    decimal riskMoney = _tab.Portfolio.ValueBegin * Risk.ValueDecimal / 100;
+                    decimal costPriceStep = _tab.Securiti.PriceStepCost;
+                    costPriceStep = 1;
+                    decimal steps = (lastUp - lastDown) / _tab.Securiti.PriceStep;
+                    decimal lot = riskMoney / (steps * costPriceStep);
+
+                   // _tab.SellAtMarket((int)lot);
+                    _tab.SellAtMarket(Lot.ValueInt);
+                }
+            }
+
+            if (positions.Count > 0)
+            {
+                Trailing(positions);
+            }
+        }
+        
+        private void Trailing(List<Position> positions)
+        {
+            decimal lastDown = _pc.DataSeries[1].Values.Last();
+            decimal lastUp = _pc.DataSeries[0].Values.Last();
+            foreach (Position pos in positions)
+            {
+                if (pos.State == PositionStateType.Open)
+                {
+                    if (pos.Direction == Side.Buy)
+                    {
+                        _tab.CloseAtTrailingStop(pos, lastDown, lastDown - 100 * _tab.Securiti.PriceStep);
+                    }
+                    if (pos.Direction == Side.Sell)
+                    {
+                        _tab.CloseAtTrailingStop(pos, lastUp, lastUp + 100 * _tab.Securiti.PriceStep);
+                    }
+                }
             }
         }
         public override string GetNameStrategyType()
